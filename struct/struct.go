df716b064e6a4a97918b5e7c643d8769ad84ab8e
Sedah <phensiritn1@gmail.com>
2022-03-24 14:43:59 +0700

    add struct, method, interface

---
+++ struct/struct.go
@@ -0,0 +1,37 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.base * t.height) * 0.5
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
