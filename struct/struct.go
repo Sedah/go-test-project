a16b84a7334ed4f332ab15b479e5217d8c4215b7
Sedah <phensiritn1@gmail.com>
2022-03-24 16:16:33 +0700

    add pointer and error

--- struct/struct.go
+++ struct/struct.go
@@ -3,35 +3,31 @@ package main
 import "math"
 
 type Rectangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
 }
 
 func (r Rectangle) Area() float64 {
-	return r.width * r.height
+	return r.Width * r.Height
 }
 
 type Circle struct {
-	radius float64
+	Radius float64
 }
 
 func (c Circle) Area() float64 {
-	return math.Pi * c.radius * c.radius
+	return math.Pi * c.Radius * c.Radius
 }
 
 type Triangle struct {
-	base   float64
-	height float64
+	Base   float64
+	Height float64
 }
 
 func (t Triangle) Area() float64 {
-	return (t.base * t.height) * 0.5
+	return (t.Base * t.Height) * 0.5
 }
 
 type Shape interface {
 	Area() float64
-}
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
 }
